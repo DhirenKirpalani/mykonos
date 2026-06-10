9ec2bd25eb22a71d5ead726e548aec76c1edda7c
Dhiren Kirpalani <dhirenkirpalani@Dhirens-MacBook-Pro.local>
2026-06-10 13:54:06 +0700

    chore: update config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -81,7 +81,7 @@ const config: Config = {
         lato: ['var(--font-lato)', 'Gill Sans', 'sans-serif'],
         futura: ['Futura PT', 'sans-serif'],
         montserrat: ['var(--font-montserrat)', 'sans-serif'],
-        playfair: ['var(--font-playfair)', 'Georgia', 'serif'],
+        playfair: ['Adobe Caslon Pro', 'var(--font-caslon)', 'Libre Caslon Text', 'Georgia', 'serif'],
         inter: ['var(--font-inter)', 'sans-serif'],
       },
       keyframes: {
